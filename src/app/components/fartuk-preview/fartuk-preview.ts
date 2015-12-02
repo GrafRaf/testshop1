7cadeb36065feaf78ec73fddf40d84edcd2baeba
Rafael <grafraf@mail.ru>
2015-12-02 21:09:21 +0600

    2

---
+++ src/app/components/fartuk-preview/fartuk-preview.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'fartuk-preview',
+  templateUrl: 'app/components/fartuk-preview/fartuk-preview.html',
+  styleUrls: ['app/components/fartuk-preview/fartuk-preview.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class FartukPreview {
+
+  constructor() {}
+
+}
---
+++ src/app/testshop1.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'testshop1-app',
+  providers: [],
+  templateUrl: 'app/testshop1.html',
+  directives: [],
+  pipes: []
+})
+export class Testshop1App {
+
+  constructor() {}
+
+}
